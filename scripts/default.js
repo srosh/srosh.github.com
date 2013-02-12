e5aee84066932fc14539598ba84af409b6ca9180
srosh <srosh@me.com>
2013-02-12 14:06:39 -0800

    jsonp fix

--- scripts/default.js
+++ scripts/default.js
@@ -1,10 +1,14 @@
-var gravatar='http://en.gravatar.com/srosh.json';
+var gravatar='http://en.gravatar.com/srosh.json?callback=setMyImage';
 
 (function($){
-	$.getJSON(gravatar, function(data) {
-	  if (data && data.entry) {
-	  	var turl = data.entry[0].thumbnailUrl;
-	  	$('img.gravatar').attr('src',turl);
-	  }
+	$(document).ready(function(){
+		$('body').append($('<script src="'+gravatar+'"/>'));
 	});
 })(jQuery)
+
+var setMyImage = function(data) {
+ if (data && data.entry) {
+  	var turl = data.entry[0].thumbnailUrl;
+  	$('img.gravatar').attr('src',turl);
+  }
+}
