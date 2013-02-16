d2191ad550501b6735a318a6d98b9db269e3e577
srosh <srosh@me.com>
2013-02-16 12:36:29 -0800

    pre.fix/scroll updat

--- scripts/default.js
+++ scripts/default.js
@@ -3,9 +3,9 @@ var gravatar='http://en.gravatar.com/srosh.json?callback=setMyImage';
 (function($){
 	$(document).ready(function(){
 		$('body').append($('<script src="'+gravatar+'"/>'));
-		$('.side-left').scrollToFixed({zIndex:1000,marginTop: -25 , limit: ($('footer').offset().top - $('.side-left').height() - 200) });
+		$('.side-left').scrollToFixed({zIndex:1000,marginTop: -25 , limit: ($('footer').offset().top - $('.side-left').height() ) });
 		// $('.side-left').scrollToFixed({marginTop: 68 , limit: ($('footer').offset().top - $('.side-left').height() - 200) });
-		$('.page-header').scrollToFixed({zIndex:0,marginTop: -313, limit: ($('footer').offset().top - $('.side-left').height() - 200) });
+		$('.page-header').scrollToFixed({zIndex:0,marginTop: -313, limit: ($('footer').offset().top - $('.side-left').height() ) });
 	});
 })(jQuery)
 
