7bb8f742c6bbc759158df665b741d92ab0c60fbf
srosh <srosh@me.com>
2013-02-17 22:47:46 -0800

    cleanup

--- scripts/default.js
+++ scripts/default.js
@@ -4,7 +4,6 @@ var gravatar='http://en.gravatar.com/srosh.json?callback=setMyImage';
 	$(document).ready(function(){
 		$('body').append($('<script src="'+gravatar+'"/>'));
 		$('.side-left').scrollToFixed({zIndex:1000,marginTop: -25 , limit: ($('footer').offset().top - $('.side-left').height() ) });
-		// $('.side-left').scrollToFixed({marginTop: 68 , limit: ($('footer').offset().top - $('.side-left').height() - 200) });
 		$('.page-header').scrollToFixed({zIndex:0,marginTop: -313, limit: ($('footer').offset().top - $('.side-left').height() ) });
 	});
 })(jQuery)
