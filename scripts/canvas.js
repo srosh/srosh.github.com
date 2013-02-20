6f4b072aa1301fca6269417b17af9518b1d3a856
srosh <srosh@me.com>
2013-02-19 22:37:06 -0800

    canv

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,34 +1,70 @@
-var handle,handleHasMoved,handleWasSelected;
-var path = new Path.Circle(view.center, 100);
-path.strokeColor = 'black';
-
-path.selected = true;
+var handle,handlr,handleHasMoved,handleWasSelected;
+//var path = new Path.Circle(view.center, 100);
+//path.strokeColor = 'black';
+//path.selected = true;
+var path,paths=[];
+console.log(project.activeLayer);
 
 function onMouseDown(event) {
 	handle = null;
-	// Do a hit test on path for handles:
-	var hitResult = path.hitTest(event.point, { handles: true, segments: true,tolerance: 10 });
+	//hittest for selected path
+	//if nothing gets back hittest on layer
+	//if nothing's hit then start a path
+	//or modify to add segments on click
+	var hitResult =  project.activeLayer.hitTest(event.point, { stroke: true, handles: true, segments: true,tolerance: 25 });
+	// var hitResult = !path ? false : path.hitTest(event.point, { handles: true, segments: true,tolerance: 10 });
 	if (hitResult) {
+		if (!hitResult.item.selected) {
+			project.activeLayer.selected = false;
+			hitResult.item.selected = true;
+		}
 		if (hitResult.type == 'handle-in') {
 			handle = hitResult.segment.handleIn;
+			handlr = hitResult.segment.handleOut;
 		} else if (hitResult.type == 'handle-out') {
 			handle = hitResult.segment.handleOut;
+			handlr = hitResult.segment.handleIn;
 		} else if (hitResult.type == 'segment') {
 			handle = hitResult.segment.point;
 			if(!handle.selected) {
 				handle.selected=true;
 			} else handleWasSelected = true;
-	console.log(handleWasSelected);
-
+		} else if (hitResult.type == 'stroke') {
+			path = null;
+			//console.log(hitResult);
+			handle = hitResult.item;
 		}
+	} else {
+		// if (path) {
+	 //        path.selected = false;
+	 //    }
+	    project.activeLayer.selected = false;
+	    // Create a new path and set its stroke color to black:
+	    path = new Path();
+	    path.add(event.point);
+	    path.strokeColor = 'black';
+
+	    // Select the path, so we can see its segment points:
+	    path.selected = true;
 	}
 }
 
 function onMouseDrag(event) {
 	if (handle) {
-		handle.x += event.delta.x;
-		handle.y += event.delta.y;
-		handleHasMoved = true;
+		if (handle instanceof Path) {
+			handle.translate(event.delta);
+		} else {
+			handle.x += event.delta.x;
+			//handlr.x -= event.delta.x;
+			handle.y += event.delta.y;
+			var a = handle.angle - 180;
+			a = (a < 0 ? a+360 : a)
+			handlr.angle = a;
+			console.log(a);
+			handleHasMoved = true;
+		}
+	} else {
+		path.add(event.point);
 	}
 }
 
@@ -42,5 +78,14 @@ function onMouseUp(event) {
 			handle.selected=false;
 		}
 		handleHasMoved = false;
+	} else if (path) {
+		path.simplify(10);
+		//path.selected = true;
 	}
+}
+
+function endPath(p){
+	paths.push(p);
+	p.simplify(25);
+
 }
